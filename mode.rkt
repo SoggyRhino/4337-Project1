0e23a715e1c9accf06cb53555f5abb648cc680cb
Ian <94922205+SoggyRhino@users.noreply.github.com>
2025-10-22 15:10:49 -0500

    Create mode.rkt

---
+++ mode.rkt
@@ -0,0 +1,17 @@
+(define prompt?
+   (let [(args (current-command-line-arguments))]
+     (cond
+       [(= (vector-length args) 0) #t]
+       [(string=? (vector-ref args 0) "-b") #f]
+       [(string=? (vector-ref args 0) "--batch") #f]
+       [else #t])))
+
+
+(define (tokenize str)
+    (tokenize-acc (string->list str) '())
+)
+
+
+(define (tokenize-acc lst acc)
+
+)
