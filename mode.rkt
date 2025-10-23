df52d5fec878830bce51bbcb52bf1cad144bddf2
Ian <94922205+SoggyRhino@users.noreply.github.com>
2025-10-22 22:51:09 -0500

    Session 1

--- mode.rkt
+++ mode.rkt
@@ -7,11 +7,39 @@
        [else #t])))
 
 
-(define (tokenize str)
-    (tokenize-acc (string->list str) '())
+(define (test str )
+     (join (string->list str) '() 0 '())
+)
+
+(define (join lst acc lastTokens lastType)
+    (cond
+        [(null? (first lst)) (#t acc)]
+        [(0 = lastType)
+            (cond
+                [(op? (first lst)) (join (rest lst) (cons (first lst) acc) 0 "")]
+                [(= (first lst) #\ ) (join (rest lst) (cons (first lst) acc) 0 "")]
+                [(num? (first lst)) (join (rest lst) (cons (first lst) acc) 1 "")]
+                [else (#f (format "Unknown character " (first lst))) ]
+            )]
+        [(1 = lastType)
+            (cond
+                 [(op? (first lst)) (join (rest lst) (cons (first lst) (cons lastTokens acc)) 0 "")]
+                 [(= (first lst) #\ ) (join (rest lst) (cons (first lst) acc) 0 "")]
+                 [(num? (first lst)) (join (rest lst) (cons (first lst) acc) 1 "")]
+                 [else (#f (format "Unknown character " (first lst))) ]
+            )]
+    )
+
+)
+
+(define (op? c)
+    (member? c '(#\+ #\- #\* #\/))
+)
+
+(define (num? c)
+    (member? c '(#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 ))
 )
 
 
-(define (tokenize-acc lst acc)
 
-)
+(test "+*2$1+$2 1")
